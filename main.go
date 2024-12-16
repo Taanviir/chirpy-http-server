f30723d32441dd437eebd6366c5e969e17e5f33d
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-12-17 00:55:31 +0400

    feat: added server request metrics and reset endpoints

--- main.go
+++ main.go
@@ -3,17 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.fileServerHits.Store(0)
+	w.WriteHeader(200)
+}
+
 func main() {
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		req.Header.Set("Content-Type", "text/plain")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	server := &http.Server{
 		Handler: mux,
